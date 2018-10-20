2dffaf6bc480ae6b590db72f853910fa3af40ce6
BelPet <43935610+BelPet@users.noreply.github.com>
2018-10-20 14:29:39 +0700

    done

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -21,17 +21,29 @@ int third_wheel(int number_system, char num){
     return third;
 }
 int whole_part(int num_sys_1, int num_sys_2, char num[13], int end){
-    long int whole_1=0;
-    int power=end, whole_2;
+    long int whole=0; int su[50];
+    int power=end;
     for(int i=0; i<end; i++){
         power--;
-        whole_1+=((int)num[i]-48)*p(num_sys_1, power);
+        whole+=((int)num[i]-48)*p(num_sys_1, power);
     }
-    while(whole_1>0){
-        whole_2=whole_1%num_sys_2;
-        if(whole_2>9){printf("%c", (char)(whole_2+55));}
-        else{printf("%d", whole_2);}
-        whole_1/=num_sys_2;
+    int i;
+    i = 0;
+    while (whole > 0) {
+        su[i] = whole % num_sys_2;
+        whole /= num_sys_2;
+        i += 1;
+    }
+    int j = i;
+    while (su[j] == 0) {
+        su[j] = -1;
+        j--;
+    }
+    for (int j = i-1; j >= 0; j--) {
+        if (su[j] != -1) {
+            if (su[j] <= 9) { printf("%d", su[j]); }
+            else { printf("%c", (char) (55 + su[j])); }
+        }
     }
     return 0;
 }
@@ -57,20 +69,24 @@ int main(){
     int of,at;
     char number[13];
     scanf(" %d %d", &of, &at);
-    if(of<2 || of>16 || at<2 || at>16){printf("Bad input\n");}else{
+    if(of<2 || of>16 || at<2 || at>16){printf("Bad input\n");}else {
         scanf(" %s", number);
-        int point=strlen(number), excess;
-        for(int i=0; i<strlen(number); i++){
-            if(number[i]!='.'){
-                number[i]=symbol_change(number[i]);
-                excess=third_wheel(of, number[i]);
-            }else{point=i;}
-        }
-        if(excess==1){printf("Bad input\n");}else{
-            whole_part( of, at, number, point);
-            if(point!=strlen(number)){
-                printf(".");
-                fraction( of, at, number, point);
+        if (number[strlen(number)-1] == '.' || number[1] == '.') { printf("Bad input\n"); }
+        else {
+            int point = strlen(number), excess = 0, amount_of_point=0;
+            for (int i = 0; i < strlen(number); i++) {
+                if (number[i] != '.') {
+                    number[i] = symbol_change(number[i]);
+                    excess += third_wheel(of, number[i]);
+                } else { point = i; amount_of_point++;}
+            }
+            if (excess > 0 || amount_of_point>1) { printf("Bad input\n"); }
+            else {
+                whole_part(of, at, number, point);
+                if (point != strlen(number)) {
+                    printf(".");
+                    fraction(of, at, number, point);
+                }
             }
         }
     }
