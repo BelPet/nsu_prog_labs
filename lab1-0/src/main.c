9c120206b32e7540a3895ed04693e424159cfa68
BelPet <43935610+BelPet@users.noreply.github.com>
2018-12-10 20:27:02 +0700

    done

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+char temp[18];
+char text[255]=" ";
+int d[255];
 
-int main()
-{
-  char temp[18];
-	fgets(temp, 18, stdin);
-
-	size_t ln = strlen(temp) - 1;
-	if (*temp && temp[ln] == '\n')
-		temp[ln] = '\0';
-
-	char text[255] = "";
-  int s=0;
-  while(s==0){
-    fgets(text, 255, stdin);
-	int d[255];
-
-	int t = strlen(temp);
-	int len = strlen(text);
-
-	int i;
+void shift(int t){
+  int i;
 	for (i = 0; i < 255; i++)
 	{
 		d[i] = t;
 	}
-
 	int count = 0;
-
 	for (i = t - 2; i >= 0; i--)
 	{
 		if (d[abs(temp[i])] == t)
@@ -39,36 +23,39 @@ int main()
 
 	if (temp[abs(t - 1)] == t)
 		d[abs(temp[t - 1])] = count + 1;
+}
 
-
-	int j = t - 1;
-	i = t - 1;
-	int start = i;
-
-	while (start < len)
+int boer_mure(int t, int finish){
+  int j = t - 1, i = t - 1, start = i, len = strlen(text), z=0;
+  while (start < len)
 	{
 		i = start;
 		j = t - 1;
-
-		printf("%d ", i + 1);
-
-		while (text[i] == temp[j])
-		{
+    printf("%d ", i + 1 +finish);
+		while (text[i] == temp[j]){
 			i--;
 			j--;
-
 			if (j == -1){ break;}
-				printf("%d ", i + 1);
+			printf("%d ", i + 1 + finish);
 		}
-
-		if (j != t - 1)
-			start += d[abs(temp[t - 1])];
-		else
-			start += d[abs(text[i])];
+		if (j != t - 1){start += d[temp[t - 1]];}
+    else {start += d[text[i]];}
 	}
-  if(j==-1){s+=1;}
-  }
-  
+  if(j!=-1){z=len;}
+  return z;
+}
 
-	return 0;
+int main()
+{
+	fgets(temp, 18, stdin);
+	size_t ln = strlen(temp) - 1;
+	if (*temp && temp[ln] == '\n'){temp[ln] = '\0';}
+  int s=0, t = strlen(temp);
+	shift(t);
+  do{
+    fgets(text, 255, stdin);
+    s=boer_mure(t, s);
+    printf("\n");
+  }while(s!=0);
+  return 0;
 }
