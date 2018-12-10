8813885ce8e6cc3951ca6f29c1fa77a7c6734286
BelPet <43935610+BelPet@users.noreply.github.com>
2018-12-10 09:17:05 +0700

    done

--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -1,5 +1,37 @@
-#include <stdlib.h>
-
-int main(void) {
-    return EXIT_SUCCESS;
+#include <stdio.h>
+int n, b[2000000];
+void sort_q(int q, int w){
+  if(w<=q){
+    return;
+  }
+  int i,j,x,k;
+  i=q;
+  j=w;
+  x=(i+j)/2;
+  int a=b[x];
+  while(i<=j){
+    while(b[i]<a)i++;
+    while(a<b[j])j--;
+    if(i<=j){
+      k=b[i];
+      b[i]=b[j];
+      b[j]=k;
+      i++;
+      j--;
+    }
+  }
+  sort_q(q,j);
+  sort_q(i,w);
+}
+int main() {
+  scanf("%d", &n);
+  int i=0;
+  while(n>i){
+    scanf("%d", &b[i]);
+    i++;
+  }
+  sort_q(0, n-1);
+  for(i=0; i<n; i++){
+    printf("%d ",b[i]);
+  }
 }
