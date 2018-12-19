41919883c3c59e5b29542378f37532a9745b8a1c
BelPet <43935610+BelPet@users.noreply.github.com>
2018-12-20 04:53:23 +0700

    done

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -40,8 +40,8 @@ int main()
 	fgets(temp, 18, file);
   int n=strlen(temp)-1;
 	if(temp[n]=='\n'){temp[n]='\0';}
-  char text[2085199];
-  fread(text, sizeof(char), 2085199, file);
+  char text[2085099];
+  fread(text, sizeof(char), 2085099, file);
 	if(strlen(text)<strlen(temp)){return 0;}
 	int d[256];
   shift(temp, d);
