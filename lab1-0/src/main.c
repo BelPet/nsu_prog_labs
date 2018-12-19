6642f665c78f1dad288493156e8c663dc6c7e3aa
BelPet <43935610+BelPet@users.noreply.github.com>
2018-12-19 19:34:44 +0700

    done

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -1,62 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char temp[18];
-char text[255]=" ";
-int d[255];
+#include <ctype.h>
 
-void shift(int t){
-  int i;
+int main()
+{
+	FILE* file = fopen("in.txt", "r");
+
+	char temp[18];
+	fgets(temp, 18, file);
+
+	size_t ln = strlen(temp) - 1;
+	if (*temp && temp[ln] == '\n')
+		temp[ln] = '\0';
+
+	char text[255] = "";
+
+	while (!feof(file))
+	{
+		char s[255];
+		if (fgets(s, 255, file) == 0)
+            	    continue;
+
+		int l = strlen(s);
+		if (l > 255)
+			return 0;
+
+		strcat(text, s);
+	}
+
+	if (strlen(text) < strlen(temp))
+	{
+		return 0;
+	}
+
+	int d[255];
+
+	int t = strlen(temp);
+	int len = strlen(text);
+
+	int i;
 	for (i = 0; i < 255; i++)
 	{
 		d[i] = t;
 	}
+
 	int count = 0;
+
 	for (i = t - 2; i >= 0; i--)
 	{
-		if (d[temp[i]] == t)
+		if (d[abs(temp[i])] == t)
 		{
-			d[temp[i]] = t - i - 1;
+			d[abs(temp[i])] = t - i - 1;
 			count++;
 		}
 	}
 
-	if (temp[t - 1] == t)
-		d[temp[t - 1]] = count + 1;
-}
+	if (temp[abs(t - 1)] == t)
+		d[abs(temp[t - 1])] = count + 1;
 
-int boer_mure(int t, int finish){
-  int j = t - 1, i = t - 1, start = i, len = strlen(text), z=0;
-  while (start < len)
+
+	int j = t - 1;
+	i = t - 1;
+	int start = i;
+
+	while (start < len)
 	{
 		i = start;
 		j = t - 1;
-    printf("%d ", i + 1 + finish);
-		while (text[i] == temp[j]){
+
+		printf("%d ", i + 1);
+
+		while (text[i] == temp[j])
+		{
 			i--;
 			j--;
-			if (j == -1){ break;}
-			printf("%d ", i + 1 + finish);
-		}
-		if (j != t - 1){start += d[temp[t - 1]];}
-    else {start += d[text[i]];}
-	}
-  if(j!=-1){z=len;}
-  return z;
-}
 
-int main()
-{
-	fgets(temp, 18, stdin);
-	//size_t
-  int ln = strlen(temp) - 1;
-	if (*temp && temp[ln] == '\n'){temp[ln] = '\0';}
-  int s=0, t = strlen(temp);
-	shift(t);
-  do{
-    fgets(text, 255, stdin);
-    s=boer_mure(t, s);
-    printf("\n");
-  }while(s!=0);
-  return 0;
+			if (j == -1)
+				break;
+
+			printf("%d ", i + 1);
+		}
+
+		if (j != t - 1)
+			start += d[abs(temp[t - 1])];
+		else
+			start += d[abs(text[i])];
+	}
+
+	fclose(file);
+
+	return 0;
 }
