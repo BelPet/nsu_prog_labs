10c4ee1e8868c1f6031732a6474aa664972faacc
BelPet <43935610+BelPet@users.noreply.github.com>
2018-12-26 23:14:26 +0700

    fix

--- lab3-0/src/main.c
+++ lab3-0/src/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-int numbers[2000000];
-void sift(int root, int bottom){
-  int max_child, done=0;
-  while((root*2+1<=bottom) && (done==0)){
+#include <malloc.h>
+
+static void swap(int *x, int *y){
+    int z=*x;
+    *x=*y;
+    *y=z;
+}
+
+static void sift(int root, int bottom, int *numbers){
+  int max_child;
+  while(root*2+1<=bottom){
     if(root*2+1==bottom)
       max_child=root*2+1;
     else {
@@ -14,35 +21,39 @@ void sift(int root, int bottom){
     }
     if(numbers[root]<numbers[max_child])
     {
-      int temp=numbers[root];
-      numbers[root]=numbers[max_child];
-      numbers[max_child]=temp;
+      swap(&numbers[root], &numbers[max_child]);
       root=max_child;
     }
     else
-      done=1;
+      return;
   }
 }
-void heap_sort(int n){
+
+static void heap_sort(int n, int *numbers){
   for(int i=(n/2)-1; i>=0; i--){
-    sift(i, n-1);
+    sift(i, n-1, numbers);
   }
   for(int i=n-1; i>=1; i--){
-    int tmp=numbers[0];
-    numbers[0]=numbers[i];
-    numbers[i]=tmp;
-    sift(0, i-1);
+    swap(&numbers[0], &numbers[i]);
+    sift(0, i-1, numbers);
   }
 }
+
 int main(){
-  int n;
+  int n=0;
   scanf("%d", &n);
+  int *numbers=(int*)malloc(n* sizeof(int));
+    if (numbers==NULL) {
+        perror("Failed");
+        return EXIT_FAILURE;
+    }
   for(int i=0; i<n; i++){
     scanf("%d", &numbers[i]);
   }
-  heap_sort(n);
+  heap_sort(n, numbers);
   for(int i=0; i<n; i++){
     printf("%d ", numbers[i]);
   }
+  free(numbers);
   return 0;
 }
