654245045716e3c0cfc2738cac9a7869b344d036
BelPet <43935610+BelPet@users.noreply.github.com>
2018-12-26 22:46:52 +0700

    fix

--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -1,36 +1,45 @@
 #include<stdio.h>
 #include<stdlib.h>
-int numbers[2000000];
-void swap(int x, int y){
-  int z=numbers[x];
-  numbers[x]=numbers[y];
-  numbers[y]=z;
+#include<malloc.h>
+static void swap(int *x, int *y){
+    int z=*x;
+    *x=*y;
+    *y=z;
 }
-void q_sort(int start, int end){
-  if(end<=start)
-    return;
-  int i=start, j=end, l=(i+j)/2, a=numbers[l];
-  while(i<=j){
-    while(numbers[i]<a){i++;}
-    while(a<numbers[j]){j--;}
-    if(i<=j){
-      swap(i, j);
-      i++;
-      j--;
+
+static void q_sort(int start, int end, int *numbers){
+    if(end<=start)
+        return;
+    int i=start, j=end, a=numbers[(i+j)/2];
+    while(i<=j){
+        while(numbers[i]<a){i++;}
+        while(a<numbers[j]){j--;}
+        if(i<=j){
+            swap(&numbers[i], &numbers[j]);
+            i++;
+            j--;
+        }
     }
-  }
-  q_sort(start, j);
-  q_sort(i, end);
+    q_sort(start, j, numbers);
+    q_sort(i, end, numbers);
 }
+
 int main(){
-  int n;
-  scanf("%d", &n);
-  for(int i=0; i<n; i++){
-    scanf("%d", &numbers[i]);
-  }
-  q_sort(0, n-1);
-  for(int i=0; i<n; i++){
-    printf("%d ",numbers[i]);
-  }
-  return 0;
+    int n=0;
+    scanf("%d", &n);
+    int *numbers;
+    numbers=(int*)malloc(n* sizeof(int));
+    if (numbers==NULL) {
+        perror("Failed");
+        return EXIT_FAILURE;
+    }
+    for(int i=0; i<n; i++){
+        scanf("%d", &numbers[i]);
+    }
+    q_sort(0, n-1, numbers);
+    for(int i=0; i<n; i++){
+        printf("%d ", numbers[i]);
+    }
+    free(numbers);
+    return 0;
 }
